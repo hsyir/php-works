658acea6e2d6d019e8d5093478540b34881566a6
Melty6154 <Mahziyartvkli@gmail.com>
2024-07-28 14:26:19 +0330

    Add files via upload

---
+++ 3.php
@@ -0,0 +1,18 @@
+<?php
+
+$a = 6;
+$b = 87654321;
+echo $a * $b;
+echo "\r\n";
+var_dump($b);
+$m = 1.24;
+var_dump($m);
+echo "\r\n";
+$g = 1.6;
+$g = (string) $g;
+var_dump($g);
+echo strpos("Make my own sunshine in a world of clouds" , "own");
+echo "\r\n";
+$x = "hava bas najavanmardane garm ast";
+echo strtoupper("$x");
+?>
