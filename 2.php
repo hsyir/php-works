32cf0f9ddd517fe22ce578051115820c84ef99e0
Melty6154 <Mahziyartvkli@gmail.com>
2024-07-28 12:17:46 +0330

    Add files via upload

---
+++ 2.php
@@ -0,0 +1,15 @@
+<?php
+
+$m = "Pneumonoultramicroscopicsilicovolcanoconiosis";
+echo strlen("Pneumonoultramicroscopicsilicovolcanoconiosis");
+echo "\r\n";
+var_dump($m);
+echo "\r\n";
+echo strpos("Brad Pitt" , "Pitt");
+echo "\r\n";
+echo str_word_count("World of Warcraft Battle for Azeroth");
+echo "\r\n";
+$a = 9568496;
+$a = (string) $a;
+var_dump($a);
+?>
