8e7ed61d268684c8c7c9b74b8191a8fa063921db
Melty6154 <Mahziyartvkli@gmail.com>
2024-07-28 14:56:31 +0330

    Add files via upload

---
+++ 4.php
@@ -0,0 +1,16 @@
+<?php
+// this is real btw be careful about radioactive materials
+$x = "Prolonged exposure to radioactive materials is not recommended";
+echo substr($x, 22, 5);
+echo "\r\n";
+echo substr($x , 22);
+echo "\r\n";
+echo substr($x, -6);
+echo "\r\n";
+// sudden hello!
+echo "Hello \"World\"!!!";
+# ah we could put comments with this little sign as well
+/*
+I didnt know that
+*/
+?>
