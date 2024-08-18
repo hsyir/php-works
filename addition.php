42f9cfc65de22004109f0a2cc9b944872b968a27
Melty6154 <Mahziyartvkli@gmail.com>
2024-08-18 18:30:59 +0330

    Add files via upload

---
+++ addition.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en" style="background-color:yellow;">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = $_GET["First"];
+        $b = $_GET["Second"];
+        echo $a + $b;
+    ?>    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,22 +1,18 @@
-<!doctype html>
-    <html style="background-color:lightgreen;">  
-        <head>  
-            <?php
-                include "header.php";
-            ?>
-        </head>
-        <body>
-            <?php 
-                echo "they are made up of several subfamilies which we'll get into";
-            ?>
-            <a href="Colubridae.php">Colubridae</a>
-            <a href="Viperidae.php">Viperidae</a>
-            <a href="Elapidae.php">Elapidae</a>
-            <a href="Other.php">Others</a>
-        </body>
-        <footer>
-            <?php
-                include "footer.php";
-            ?>    
-        </footer>
-    </html>        
+<!DOCTYPE html>
+<html lang="en" style="background-color:yellow;">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="./addition.php">
+        <label for="First">First Number:</label><br>
+        <input type="text" name="First" id="First"><br>
+        <p>+</p><br>
+        <label for="Second">Second Number:</label><br>
+        <input type="text" name="Second" id="Second"><br>
+        <input type="Submit" value="Calculate">
+    </form>        
+</body>
+</html>
